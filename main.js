83058121097c3a4050c3ccf1e683b60dfa898eb3
sabof <esabof@gmail.com>
2014-04-16 04:41:55 +0100

    WIP

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function() {
   var flickrPlugin = {};
 
-  flickrPlugin.utils = {
+  var utils = flickrPlugin.utils = {
     runHook: function(hookName) {
       var args = Array.prototype.slice.apply(arguments, 1);
       var hooks = this.hooks[hookName] || [];
@@ -19,6 +19,12 @@
       object.hooks = {};
       object.addHook = this.addHook;
       object.runHook = this.runHook;
+    },
+
+    removeAllChildren: function(domElement) {
+      while (domElement.firstChild) {
+        domElement.removeChild(domElement.firstChild);
+      }
     }
   };
 
@@ -30,20 +36,25 @@
     this.data = data;
   };
 
-  FlickrImageModel.prototype.getThumbnail = function(pageNumber) {
-  };
-
   FlickrImageModel.prototype.getId = function() {
     return this.data.id;
   };
 
   FlickrImageModel.prototype.getImage = function(pageNumber) {
     var d = this.data;
-    // (format "http://farm%s.staticflickr.com/%s/%s_%s_z.jpg" farm server id secret)
     return "http://farm" + d.farm + ".staticflickr.com/" +
       d.server + "/" +
       d.id + "_" +
       d.secret + "_z.jpg";
+  };
+
+  FlickrImageModel.prototype.getThumbnail = function(pageNumber) {
+    var d = this.data;
+    // (format "http://farm%s.staticflickr.com/%s/%s_%s_z.jpg" farm server id secret)
+    return "http://farm" + d.farm + ".staticflickr.com/" +
+      d.server + "/" +
+      d.id + "_" +
+      d.secret + "_s.jpg";
   };
 
   // ---------------------------------------------------------------------------
@@ -80,7 +91,31 @@
     this.runHook('currentImageChanged', image);
   };
 
-  FlickrPluginModel.prototype.setCurrentPage = function(pageNumber) {
+  FlickrPluginModel.prototype.getCurrentPage = function() {
+    return this.currentPage;
+  };
+
+  FlickrPluginModel.prototype.getTotalPages = function() {
+    return this.totalPages;
+  };
+
+  FlickrPluginModel.prototype.gotoLastPage = function(pageNumber) {
+    this.gotoPage(this.totalPages);
+  };
+
+  FlickrPluginModel.prototype.gotoFirstPage = function(pageNumber) {
+    this.gotoPage(1);
+  };
+
+  FlickrPluginModel.prototype.gotoPage = function(pageNumber) {
+    this.currentPage = pageNumber;
+    if (this.lastSeachString) {
+      this.search();
+    }
+    this.runHook('currentPageChanged', this.currentPage);
+  };
+
+  FlickrPluginModel.prototype.gotoPage = function(pageNumber) {
     this.currentPage = pageNumber;
     if (this.lastSeachString) {
       this.search();
@@ -131,52 +166,91 @@
 
   // ---------------------------------------------------------------------------
 
-  var FlickrPlugin = flickrPlugin.FlickrPlugin = function(
-    domElement, apiKey
+  var FlickrPluginPagerView = flickrPlugin.FlickrPluginPagerView = function(
+    domElement, model
   ) {
-    flickrPlugin.utils.extendWithHooks(this);
-    this.currentPage = 0;
-    this.totalPages = 0;
-    this.itemsPerPage = 6;
-    this.gridDomRooot = null;
-    this.imageDomRooot = null;
-    this.currentData = null;
-    this.apiKey = apiKey;
+    flickrPlugin.addHook('totalPagesChanged', this._render, this);
+    flickrPlugin.addHook('currentPageChanged', this._render, this);
+    this.domRoot = domElement;
+    this.radius = 5;
   };
 
-  FlickrPlugin.prototype._setContent = function(data) {
-    this.setCurrentPage();
-  };
+  FlickrPluginPagerView.prototype._render = function() {
+    var root = this.domRoot;
+    utils.removeAllChildren(root);
+    var totalPages = this.model.getTotalPages();
+    var currentPage = this.model.getCurrentPage();
 
-  FlickrPlugin.prototype._populate = function(data) {
-    this.setCurrentPage();
+    var firstPage = Math.max(1, currentPage - this.radius);
+    var lastPage = Math.min(totalPages, currentPage + this.radius);
+
+    var start = document.createElement('span');
+    var end = document.createElement('span');
+
+    start.appendChild(
+       document.createTextNode('<<')
+    );
+    start.classList.add('start');
+    root.appendChild(start);
+
+    for (var i = firstPage; i <= lastPage; i++) {
+      var number = document.createElement('span');
+      number.appendChild(
+        document.createTextNode(i)
+      );
+      number.setAttribute('data-number', i);
+      root.appendChild(number);
+    }
+
+    end.appendChild(
+       document.createTextNode('>>')
+    );
+    end.classList.add('end');
+    root.appendChild(end);
   };
 
   // ---------------------------------------------------------------------------
 
-  var FlickrPluginPager = flickrPlugin.FlickrPluginPager = function(
-    // FIXME?: Change to "pageable"
+  var FlickrPluginGridView = flickrPlugin.FlickrPluginGridView = function(
     domElement, model
   ) {
-    flickrPlugin.addHook('totalPagesChanged', this.setTotalPages, this);
     flickrPlugin.addHook('currentPageChanged', this.setTotalPages, this);
+    this.domRoot = domElement;
+    this.model = model;
   };
 
-  FlickrPluginPager.prototype.render = function() {
-    // this.model.currentPage;
+  FlickrPluginGridView.prototype._render = function() {
+    var images = this.model.getImages();
+    var root = this.domRoot;
+    utils.removeAllChildren(root);
+
+    images.forEach(function(imageObject) {
+      var imageContainer = document.createElement('div');
+      var self = this;
+      var imageLink = document.createElement('a');
+      imageLink.href = '#';
+
+      (function(id) {
+        imageLink.onclick = function() {
+          self.setCurrentImage(id);
+        };
+      }(imageObject.getId()));
+
+      imageContainer.appendChild(imageLink);
+
+      var image = document.createElement('img');
+      imageLink.appendChild(image);
+      image.src = imageObject.getImage();
+
+      root.appendChild(imageContainer);
+    });
   };
 
-  FlickrPluginPager.prototype.setCurrentPage = function(pageNumber) {
-    this.pageNumber = pageNumber;
-  };
-
-  FlickrPluginPager.prototype.setTotalPages = function(pageNumber) {
-
-  };
-
-  FlickrPluginPager.prototype.setTotalPages = function(pageNumber) {
+  window.initFlickrPlugin = function(mainImageDom, pagerDom, gridDom) {
+    var model = new FlickrPluginModel('3ed0a4d16d372c4464a49ada6d6212b0');
+    var pager = new FlickrPluginPagerView(pagerDom, model);
+    var grid = new FlickrPluginGridView(gridDom, model);
 
   };
 
-  window.flickrPlugin = flickrPlugin;
 }());
